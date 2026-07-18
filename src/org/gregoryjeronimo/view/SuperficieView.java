a15021b6f9bf8264aefb715aa7f7ad982a03b196
Gregory Jeronimo <gjeronimo-2026116@kinal.edu.gt>
2026-07-18 12:43:57 -0600

    011 Se ha editado clase SuperficieView

--- src/org/gregoryjeronimo/view/SuperficieView.java
+++ src/org/gregoryjeronimo/view/SuperficieView.java
@@ -1,10 +1,14 @@
 
 package org.gregoryjeronimo.view;
 
+import java.util.ArrayList;
 /**
  *
  * @author Gregory Jeronimo
  */
-public class SuperficieView {
+public class SuperficieView extends Aplication {
+    private ArrayList<Superficie> listaSuperficies = new ArrayList<>();
     
+    private ListView<Superficie> listViewResumen = new ListView<>();
+    private Label lblTotalCosto = new Label("Total Cotización: Q.0.00");
 }
