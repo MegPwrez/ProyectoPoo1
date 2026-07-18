d586ed7a2a3c4859ef824a326f380488547da142
Gregory Jeronimo <gjeronimo-2026116@kinal.edu.gt>
2026-07-18 12:05:42 -0600

    002 Se a agregado getter y setters en clase Superficie.

--- src/org/gregoryjeronimo/model/Superficie.java
+++ src/org/gregoryjeronimo/model/Superficie.java
@@ -8,6 +8,27 @@ package org.gregoryjeronimo.model;
  *
  * @author Amanda Pérez
  */
-public class Superficie {
+public abstract class Superficie {
+    private String tipo; // Atributo común
+
+    public Superficie(String tipo) {
+        this.tipo = tipo;
+    }
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+    public abstract double calcularArea();
+    
+  
+
+    @Override
+    public String toString() {
+        return String.format("%s - Área: %.2f m²", tipo, calcularArea());
+    }
+
     
 }
