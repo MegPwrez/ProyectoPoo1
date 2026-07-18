9115f4d606819943534ea233fe1f04da42be7467
Gregory Jeronimo <gjeronimo-2026116@kinal.edu.gt>
2026-07-18 12:34:29 -0600

    009 Se ha editado clase Triangulo

--- src/org/gregoryjeronimo/model/Triangulo.java
+++ src/org/gregoryjeronimo/model/Triangulo.java
@@ -1,13 +1,27 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package org.gregoryjeronimo.model;
 
 /**
  *
- * @author Amanda Pérez
+ * @author Gregory Jeronimo
  */
-public class Triangulo {
+public class Triangulo extends Superficie {
+    private double base;
+    private double altura;
+
+    public Triangulo(double base, double altura) {
+        super("Triángulo");
+        this.base = base;
+        this.altura = altura;
+    }
+    public double getBase() { return base; }
+    public void setBase(double base) { this.base = base; }
+    public double getAltura() { return altura; }
+    public void setAltura(double altura) { this.altura = altura; }
+
+    @Override
+    public double calcularArea() {
+        return (base * altura) / 2.0;
+    }
     
 }
