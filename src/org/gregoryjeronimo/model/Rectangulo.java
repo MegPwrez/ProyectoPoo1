e792924c0835e0e228492cdf77325cc9da14110a
Gregory Jeronimo <gjeronimo-2026116@kinal.edu.gt>
2026-07-18 12:28:06 -0600

    007 Se ha editado clase Rectangulo

--- src/org/gregoryjeronimo/model/Rectangulo.java
+++ src/org/gregoryjeronimo/model/Rectangulo.java
@@ -5,6 +5,22 @@ package org.gregoryjeronimo.model;
  *
  * @author Gregory Jeronimo
  */
-public class Rectangulo {
-    
+public class Rectangulo extends Superficie {
+    private double base;
+    private double altura;
+
+    public Rectangulo(double base, double altura) {
+        super("Rectángulo");
+        this.base = base;
+        this.altura = altura;
+    }
+    public double getBase() { return base; }
+    public void setBase(double base) { this.base = base; }
+    public double getAltura() { return altura; }
+    public void setAltura(double altura) { this.altura = altura; }
+
+    @Override
+    public double calcularArea() {
+        return base * altura;
+    }
 }
