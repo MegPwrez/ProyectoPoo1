ffb42239f48696c7665310b16c6d177d45519c99
Gregory Jeronimo <gjeronimo-2026116@kinal.edu.gt>
2026-07-18 12:21:40 -0600

    004 Se ha agregado clase Circulo en model

---
+++ src/org/gregoryjeronimo/model/Circulo.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package org.gregoryjeronimo.model;
+
+/**
+ *
+ * @author Amanda Pérez
+ */
+public class Circulo extends Superficie {
+    private double radio; // Atributo único
+
+    public Circulo(double radio) {
+        super("Círculo");
+        this.radio = radio;
+    }
+    
+    
+}
