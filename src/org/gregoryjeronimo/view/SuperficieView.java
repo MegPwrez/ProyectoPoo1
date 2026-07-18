7a51828329a07e0d9599eb8f210d2659aae08b4a
Gregory Jeronimo <gjeronimo-2026116@kinal.edu.gt>
2026-07-18 12:53:16 -0600

    012 Se ha agregado metodos clase SuperficieView

--- src/org/gregoryjeronimo/view/SuperficieView.java
+++ src/org/gregoryjeronimo/view/SuperficieView.java
@@ -1,6 +1,17 @@
 
 package org.gregoryjeronimo.view;
 
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import modelo.Superficie;
+import modelo.Circulo;
+import modelo.Rectangulo;
+import modelo.Triangulo;
 import java.util.ArrayList;
 /**
  *
@@ -11,4 +22,28 @@ public class SuperficieView extends Aplication {
     
     private ListView<Superficie> listViewResumen = new ListView<>();
     private Label lblTotalCosto = new Label("Total Cotización: Q.0.00");
+
+    private static final double PRECIO_METRO_CUADRADO = 50.00;
+
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("La Constructora - Cálculo de Materiales");
+
+     
+        TabPane tabPane = new TabPane();
+        Tab tabRegistro = new Tab("Registro");
+        Tab tabResumen = new Tab("Resumen");
+        tabRegistro.setClosable(false);
+        tabResumen.setClosable(false);
+        
+        VBox rootRegistro = new VBox(15);
+        rootRegistro.setPadding(new Insets(20));
+
+        Label lblSeleccion = new Label("Seleccione el tipo de superficie:");
+        ComboBox<String> comboFormas = new ComboBox<>();
+        comboFormas.getItems().addAll("Circulo", "Rectangulo", "Triangulo");
+        
+        GridPane gridFormulario = new GridPane();
+        gridFormulario.setHgap(10);
+        gridFormulario.setVgap(10);
 }
